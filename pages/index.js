1eff0c274c1713a03e338968c151c3b0b6a568f2
gabriellengoo <gabriellengoo@hotmail.com>
2022-06-19 00:26:37 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -175,7 +175,7 @@ export default function Home({ posts }) {
 {/* <iframe className={styles.video} src="https://drive.google.com/file/d/14w2FfWc-LfdUSPoM79y06_sdVXXG_QXa/preview?start=1"></iframe> */}
 {/* style="transform: translateY(-38.2813%);" style="position:absolute;top:0;left:0;width:100%;height:100%;" */}
 {/* <div class="video-wrapper"><div class="plyr__video-embed__container" > */}
-          <iframe src="https://player.vimeo.com/video/721799699?h=b659979974?background=1&amp;autoplay=1&amp;muted=1&amp;loop=1&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" className={styles.video} allow="autoplay; fullscreen; picture-in-picture; loop=1; muted=1" id="video" muted webkitallowfullscreen mozallowfullscreen allowfullscreen title="fixed metaverse"></iframe>            
+          <iframe src="https://player.vimeo.com/video/721799699?h=b659979974?background=1&amp;autoplay=1&amp;muted=1&amp;loop=1&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameBorder="0" className={styles.video} allow="autoplay; fullscreen; picture-in-picture; loop=1; muted=1" id="video" muted webkitallowfullscreen mozallowfullscreen allowFullScreen title="fixed metaverse"></iframe>            
           {/* <div class="plyr__captions"></div> */}
       {/* </div> */}
 </div>
